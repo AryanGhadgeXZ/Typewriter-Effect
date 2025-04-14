ebfba06f996e1abddc1d689ee8e87ca4c47da497
DrZombie <54247220+AryanGhadgeXZ@users.noreply.github.com>
2025-04-14 23:07:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-// const containerEl = document.querySelector(".container");
+const containerEl = document.querySelector(".container");
 
-// const careers = [
-//   "Zombie",
-//   "Web Developer",
-//   "Freelancer",
-//   "Fighter",
-//   "Illustrator",
-// ];
+const careers = [
+  "Zombie",
+  "Web Developer",
+  "Freelancer",
+  "Fighter",
+  "Illustrator",
+];
 
-// let careerIndex = 0;
-// let characterIndex = 0;
+let careerIndex = 0;
+let characterIndex = 0;
 
-// updateText();
+updateText();
 
-// function updateText() {
-//   characterIndex++;
-//   containerEl.innerHTML = `
-//     <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
-//     careerIndex
-//   ].slice(0, characterIndex)}</h1>
-//     `;
-//   if (characterIndex === careers[careerIndex].length) {
-//     careerIndex++;
-//     characterIndex = 0;
-//   }
-//   if (careerIndex === careers.length) {
-//     careerIndex = 0;
-//   }
-//   setTimeout(updateText, 200);
-// }
+function updateText() {
+  characterIndex++;
+  containerEl.innerHTML = `
+    <h1>I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>
+    `;
+  if (characterIndex === careers[careerIndex].length) {
+    careerIndex++;
+    characterIndex = 0;
+  }
+  if (careerIndex === careers.length) {
+    careerIndex = 0;
+  }
+  setTimeout(updateText, 200);
+}
